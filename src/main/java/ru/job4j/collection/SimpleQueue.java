b17398c45d814f3bd40ebd3768ca20bfb1c62a08
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-09-16 22:03:02 +0300

    5. Очередь на двух стеках [#160 #203560] fix

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -4,22 +4,16 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
-    int size;
-
     public T poll() {
-        for (int i = 0; i < size; i++) {
-            out.push(in.pop());
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
         }
-        T first = out.pop();
-        size--;
-        for (int i = 0; i < size; i++) {
-            in.push(out.pop());
-        }
-        return first;
+        return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
-        size++;
     }
 }
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -4,11 +4,24 @@ package ru.job4j.collection;
 public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<>();
 
+    private int size;
+
     public T pop() {
-        return linked.deleteFirst();
+        T value = linked.deleteFirst();
+        size--;
+        return value;
     }
 
     public void push(T value) {
         linked.addFirst(value);
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
