c2b4538418decbdadd1ab3b8d0d132fd9d8ecb7d
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-09-18 09:40:47 +0300

    1. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,39 @@
+package ru.job4j.map;
+
+import java.time.LocalDate;
+
+public class User {
+    private String name;
+    private int children;
+    private LocalDate birthday;
+
+    public User(String name, int children, LocalDate birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+}
