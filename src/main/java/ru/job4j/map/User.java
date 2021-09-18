f6a5c1fa42bbdc92510db316679b39f581cdd2d7
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-09-18 10:12:46 +0300

    4. Переопределить только equals [#1004]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -41,9 +41,23 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
     }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, children, birthday);
+//    }
 
     public static void main(String[] args) {
         User user1 = new User("Ivan", 0, LocalDate.of(2020, 10, 10));
