c27998be22ba921824291199c7a3aa1d0fa6bcc1
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-10-02 17:05:43 +0300

    0.1. FileOutputStream. [#252488 #211335] fix

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,31 +1,18 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class ResultFile {
     public static void main(String[] args) {
-        int[][] matrix = multiple(9);
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            for (int[] ints : matrix) {
-                for (int anInt : ints) {
-                    out.write(String.format("%3d", anInt).getBytes());
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    out.write(String.format("%3d", (i + 1) * (j + 1)).getBytes());
                 }
                 out.write(System.lineSeparator().getBytes());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private static int[][] multiple(int size) {
-        int[][] matrix = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = (i + 1) * (j + 1);
-            }
-        }
-        return matrix;
     }
 }
