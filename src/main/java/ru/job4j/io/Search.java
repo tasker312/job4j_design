e32804a85471496c590200e43a4f8d904743c2ff
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-10-04 20:39:01 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -3,14 +3,16 @@ package ru.job4j.io;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("js")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Invalid arguments. "
+                    + "Use java -jar dir.jar <ROOT_FOLDER> <FILE_EXTENSION>");
+        }
+        search(Path.of(args[0]), p -> p.toFile().getName().endsWith("." + args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
