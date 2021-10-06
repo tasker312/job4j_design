71498a2b7b7a9d655c0ae1675e76f9de8c24f5d4
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-10-06 13:28:34 +0300

    5. Валидация параметров запуска. [#246865 #212153] fix

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,11 +8,24 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        validation(args);
+        search(Path.of(args[0]), p -> p.toFile().getName().endsWith("." + args[1])).forEach(System.out::println);
+    }
+
+    private static void validation(String[] args) {
         if (args.length != 2) {
             throw new IllegalArgumentException("Invalid arguments. "
-                    + "Use java -jar dir.jar <ROOT_FOLDER> <FILE_EXTENSION>");
+                    + "Use java -jar Search.jar <ROOT_FOLDER> <FILE_EXTENSION>");
         }
-        search(Path.of(args[0]), p -> p.toFile().getName().endsWith("." + args[1])).forEach(System.out::println);
+        Path path = Path.of(args[0]);
+        if (!path.toFile().exists()) {
+            throw new IllegalArgumentException(
+                    String.format("Root does not exist %s", path.toFile().getAbsoluteFile()));
+        }
+        if (!path.toFile().isDirectory()) {
+            throw new IllegalArgumentException(
+                    String.format("Root is not directory %s", path.toFile().getAbsoluteFile()));
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
