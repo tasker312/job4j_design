b1e5124c0a1bd1aa0a0baaf3776d4821381bb36d
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-10-08 05:00:10 +0300

    4.2. Поиск дубликатов [#315066 #212126] fix

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,12 +3,18 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor visitor = new DuplicatesVisitor();
         Files.walkFileTree(Path.of("D:\\1"), visitor);
         var duplicates = visitor.getFiles();
+        printDuplicates(duplicates);
+    }
+
+    private static void printDuplicates(Map<FileProperty, List<Path>> duplicates) {
         duplicates.entrySet().stream()
                 .filter(entry -> entry.getValue().size() > 1)
                 .forEach(entry -> {
