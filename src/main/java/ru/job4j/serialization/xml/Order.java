e103887ae9cbb14d77c405b1e77ebc017f318c30
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-10-11 13:17:37 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

---
+++ src/main/java/ru/job4j/serialization/xml/Order.java
@@ -0,0 +1,72 @@
+package ru.job4j.serialization.xml;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
+
+@XmlRootElement(name = "order")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Order {
+
+    @XmlElement
+    private double price;
+
+    @XmlElement
+    private String comment;
+
+    @XmlElement
+    private Person client;
+
+    @XmlElementWrapper(name = "products")
+    @XmlElement(name = "product")
+    private Product[] products;
+
+    @XmlElement
+    private boolean isFinished;
+
+    public Order() {
+    }
+
+    public Order(String comment, Person client, Product[] products) {
+        this.comment = comment;
+        this.client = client;
+        this.products = products;
+        Arrays.stream(products).forEach(product -> price += product.getPrice());
+        isFinished = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{"
+                + "price=" + price
+                + ", comment='" + comment + '\''
+                + ", client=" + client
+                + ", products=" + Arrays.toString(products)
+                + ", isFinished=" + isFinished
+                + '}';
+    }
+
+    public static void main(String[] args) throws Exception {
+        Order order = new Order("hello", new Person("Ivan", "88888888888"),
+                new Product[]{new Product("Milk", 20.5), new Product("Chocolate", 40)});
+        System.out.println(order);
+        JAXBContext context = JAXBContext.newInstance(Order.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml;
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(order, writer);
+            xml = writer.getBuffer().toString();
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Order result = (Order) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+        System.out.println(xml);
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/xml/Product.java
@@ -0,0 +1,41 @@
+package ru.job4j.serialization.xml;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "product")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Product {
+
+    @XmlElement
+    private String name;
+
+    @XmlElement
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{"
+                + "name='" + name + '\''
+                + ", price=" + price
+                + '}';
+    }
+}
