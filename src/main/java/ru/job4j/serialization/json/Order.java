c3beb263785ba2bae26b7c8c287473aeb5e394d7
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-10-12 03:27:56 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/Order.java
+++ src/main/java/ru/job4j/serialization/json/Order.java
@@ -2,6 +2,8 @@ package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -20,6 +22,26 @@ public class Order {
         isFinished = false;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Person getClient() {
+        return client;
+    }
+
+    public Product[] getProducts() {
+        return products;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
     @Override
     public String toString() {
         return "Order{"
@@ -34,11 +56,37 @@ public class Order {
     public static void main(String[] args) {
         Order order = new Order("hello", new Person("Ivan", "88888888888"),
                 new Product[]{new Product("Milk", 20.5), new Product("Chocolate", 40)});
+        usingGson(order);
+        usingJson(order);
+    }
+
+    private static void usingGson(Order order) {
         System.out.println(order);
         final Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(order);
         Order newOrder = gson.fromJson(json, Order.class);
         System.out.println(newOrder);
         System.out.println(json);
+    }
+
+    private static void usingJson(Order order) {
+        System.out.println(new JSONObject(order));
+
+        JSONObject jsonObject = new JSONObject()
+                .put("price", order.price)
+                .put("comment", order.comment)
+                .put("client", new JSONObject()
+                        .put("name", order.client.getName())
+                        .put("phone", order.client.getPhone()))
+                .put("products", new JSONArray()
+                        .put(new JSONObject()
+                                .put("name", order.products[0].getName())
+                                .put("price", order.products[0].getPrice()))
+                        .put(new JSONObject()
+                                .put("name", order.products[1].getName())
+                                .put("price", order.products[1].getPrice())))
+                .put("isFinished", order.isFinished);
+        System.out.println(jsonObject);
+
     }
 }
